770ffec4beaa55be74da1464dd6e56c916c0bf24
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-03-28 10:31:09 +0100

    feat: Add responsiveness to the hero section

--- src/components/homePage/Hero.jsx
+++ src/components/homePage/Hero.jsx
@@ -3,11 +3,11 @@ import Hero_background from "@/assets/images/Hero_background.webp";
 function Hero() {
   return (
     <section>
-      <div className="relative mx-12">
+      <div className="relative mx-5 md:mx-8 lg:mx-12">
         <img
           src={Hero_background}
           alt="Hero section image"
-          className="w-full h-auto max-h-[600px] object-cover rounded-3xl"
+          className="w-full h-auto max-h-[200px] md:max-h-[400px] lg:max-h-[600px] object-cover rounded-3xl"
         />
 
         <div className="absolute inset-0 rounded-3xl bg-black/30"></div>
