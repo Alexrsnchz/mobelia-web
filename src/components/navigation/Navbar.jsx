61a958e826dd89e59a88d3d7c4ed7068ee239bb0
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-04-01 22:47:32 +0200

    fix: Change navbar spacing values to fix large screens responsive issue

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -8,26 +8,38 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { CartIcon, HeartIcon, UserIcon } from "@/components/icons/Icons.jsx";
-import NavbarLink from "@/components/navigation/NavbarLink.jsx";
+import HoverLink from "@/components/navigation/HoverLink.jsx";
 import MobeliaLogo_badge from "@/assets/images/MobeliaLogo_badge.webp";
 import ResponsiveMenu from "@/components/navigation/ResponsiveMenu.jsx";
 
 function Navbar() {
+  const livingRoom = [
+    { text: "Sofas & Couches", route: "/sofas&couches" },
+    { text: "Coffee tables", route: "/coffee-tables" },
+    { text: "TV Stands", route: "/tv-stands" },
+    { text: "Accent chairs", route: "/accent-chairs" },
+    { text: "Bookcases", route: "/bookcases" },
+  ];
+
   return (
-    <nav className="sticky flex justify-between items-center top-0 z-50 max-w-7xl mx-auto px-8 md:px-16 lg:px-0 py-4 bg-white">
+    <nav className="sticky flex justify-between items-center top-0 z-50 max-w-7xl mx-auto px-8 md:px-16 xl:px-24 2xl:px-0 py-4 bg-white">
       <Link to="/" className="flex items-center gap-2">
         <img src={MobeliaLogo_badge} alt="Möbelia logo" className="w-10 h-6" />
         <span className="text-xl font-bold">Möbelia</span>
       </Link>
 
       <div className="hidden lg:flex items-center space-x-8">
-        <NavbarLink text="Living room" />
-        <NavbarLink text="Bedroom" />
-        <NavbarLink text="Dining" />
-        <NavbarLink text="Bathroom" />
-        <NavbarLink text="Office" />
-        <NavbarLink text="Outdoor" />
-        <NavbarLink text="Decor" />
+        <HoverLink
+          text="Living room"
+          category="living-room"
+          items={livingRoom}
+        />
+        <HoverLink text="Bedroom" category="bedroom" items={livingRoom} />
+        <HoverLink text="Dining" category="dining-room" items={livingRoom} />
+        <HoverLink text="Bathroom" category="bathroom" items={livingRoom} />
+        <HoverLink text="Office" category="home-office" items={livingRoom} />
+        <HoverLink text="Outdoor" category="outdoor" items={livingRoom} />
+        <HoverLink text="Decor" category="decoration" items={livingRoom} />
       </div>
 
       <div className="hidden lg:flex items-center space-x-10">
