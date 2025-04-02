8f90e51b725732823372a0c88bf74bbd14c89006
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-04-02 21:21:25 +0200

    style: Change spacing class in nav logo component for coherence

--- src/components/navigation/NavLogo.jsx
+++ src/components/navigation/NavLogo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 function NavLogo() {
   return (
-    <Link to="/" className="flex items-center gap-2">
+    <Link to="/" className="flex items-center space-x-2">
       <img src={MobeliaLogo_badge} alt="Möbelia logo" className="w-10 h-6" />
       <span className="text-xl font-bold">Möbelia</span>
     </Link>
