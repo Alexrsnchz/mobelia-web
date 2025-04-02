b2c5fe0dcd56e0ae462eac45b5b81815f4e90363
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-04-02 17:18:37 +0200

    styles: Change Navbar links menu styling

---
+++ src/components/navigation/HoverLink.jsx
@@ -0,0 +1,35 @@
+import { ChevronDownIcon } from "@/components/icons/Icons.jsx";
+import { Link } from "react-router";
+import { Separator } from "@/components/ui/separator.jsx";
+
+function HoverLink({ text, category, items = [] }) {
+  return (
+    <div className="relative flex items-center gap-1 cursor-pointer group">
+      <span className="text-sm font-medium">{text}</span>
+      <ChevronDownIcon className="w-4 h-4 mt-1 text-gray-500 group-hover:rotate-180 transition-transform duration-200 ease-in-out" />
+
+      <div className="absolute invisible flex flex-col top-full pt-5 w-[200px] group-hover:visible rounded-sm bg-white">
+        {items.map((item, index) => (
+          <Link
+            key={index}
+            to={item.route}
+            className="px-3 py-2 w-full text-gray-800 hover:bg-gray-200"
+          >
+            {item.text}
+          </Link>
+        ))}
+
+        <Separator />
+
+        <Link
+          to={"/furniture/" + category}
+          className="px-3 py-2 w-full font-semibold text-gray-800"
+        >
+          See all
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default HoverLink;
