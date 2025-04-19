c07b27399496a5cbe3f4b08dcaedc82f5d520a65
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-04-19 18:20:49 +0200

    style: Fix more spacing issues in services section

--- src/components/website/homePage/Hero.jsx
+++ src/components/website/homePage/Hero.jsx
@@ -3,14 +3,14 @@ import Hero_background from "@/assets/images/Hero_background.webp";
 function Hero() {
   return (
     <section>
-      <div className="relative mx-5 md:mx-8 lg:mx-12">
+      <div className="relative md:mx-8 lg:mx-12">
         <img
           src={Hero_background}
           alt="Hero section image"
-          className="w-full h-auto max-h-[200px] md:max-h-[400px] lg:max-h-[600px] object-cover rounded-3xl"
+          className="w-full h-auto max-h-[200px] md:max-h-[400px] lg:max-h-[600px] object-cover md:rounded-3xl"
         />
 
-        <div className="absolute inset-0 rounded-3xl bg-black/30"></div>
+        <div className="absolute inset-0 md:rounded-3xl bg-black/30"></div>
       </div>
     </section>
   );
--- src/components/website/homePage/Services.jsx
+++ src/components/website/homePage/Services.jsx
@@ -9,7 +9,7 @@ export default function Services() {
   return (
     <section id="services">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 bg-stone-100">
-        <div className="flex flex-col items-center px-14 py-12 md:px-16 md:py-12 lg:p-12">
+        <div className="flex flex-col items-center px-14 py-12 md:px-18 md:py-12 lg:p-12">
           <div className="p-4 rounded-full bg-stone-300">
             <ToolsIcon className="w-7 h-7 text-gray-600" />
           </div>
@@ -18,24 +18,24 @@ export default function Services() {
               Assembly Included
             </h3>
             <p className="text-center font-medium text-gray-700">
-              Our team will install it right in your home with 0 hassles.
+              Our team will install it for you with 0 hassles.
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-center px-14 py-12 md:px-16 md:py-12 lg:p-12">
+        <div className="flex flex-col items-center px-14 py-12 md:px-18 md:py-12 lg:p-12">
           <div className="p-4 rounded-full bg-stone-300">
             <TruckIcon className="w-7 h-7 text-gray-600" />
           </div>
           <div className="mt-5 space-y-3">
             <h3 className="text-center text-xl font-semibold text-gray-800">
-              Fast & Secure Delivery
+              Fast Delivery
             </h3>
             <p className="text-center font-medium text-gray-700">
               Receive your order safely in 3-5 business days.
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-center px-14 py-12 md:px-16 md:py-12 lg:p-12">
+        <div className="flex flex-col items-center px-14 py-12 md:px-18 md:py-12 lg:p-12">
           <div className="p-4 rounded-full bg-stone-300">
             <ShieldIcon className="w-7 h-7 text-gray-600" />
           </div>
@@ -48,7 +48,7 @@ export default function Services() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-center px-14 py-12 md:px-16 md:py-12 lg:p-12">
+        <div className="flex flex-col items-center px-14 py-12 md:px-18 md:py-12 lg:p-12">
           <div className="p-4 rounded-full bg-stone-300">
             <BoxIcon className="w-7 h-7 text-gray-600" />
           </div>
