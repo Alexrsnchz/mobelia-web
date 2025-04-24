ec205fe51a3b4a74f2c49b3be2c190df42bb2ace
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-04-24 23:22:00 +0200

    refactor: Remove unnecessary code

--- src/components/website/homePage/arrivals/Arrivals.jsx
+++ src/components/website/homePage/arrivals/Arrivals.jsx
@@ -1,16 +1,9 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router";
-import { ArrowRightIcon } from "@/components/icons/Icons.jsx";
 import arrivals from "@/assets/data/arrivalsData.js";
+import { ArrowRightIcon } from "@/components/icons/Icons.jsx";
 import ArrivalsCarousel from "@/components/website/homePage/arrivals/ArrivalsCarousel.jsx";
 
 export default function Arrivals() {
-  const [furniture, setFurniture] = useState([]);
-
-  useEffect(() => {
-    setFurniture(arrivals);
-  }, []);
-
   return (
     <section id="arrivals">
       <div className="mx-auto max-w-8xl pt-12">
@@ -30,7 +23,7 @@ export default function Arrivals() {
         </div>
 
         <div className="px-2 md:px-10 pt-3 lg:pt-5">
-          <ArrivalsCarousel products={furniture} />
+          <ArrivalsCarousel products={arrivals} />
         </div>
       </div>
     </section>
