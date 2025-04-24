b4557fc3655ed0b722a20ee056760a0b38da1e61
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-04-24 19:37:11 +0200

    fix: Remove featured product and arrivals sections to improve their performance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import Newsletter from "@/components/website/homePage/Newsletter.jsx";
 import Testimonials from "@/components/website/homePage/testimonials/Testimonials.jsx";
 import Services from "@/components/website/homePage/Services.jsx";
 import SalesBanner from "@/components/website/homePage/SalesBanner.jsx";
-import Arrivals from "@/components/website/homePage/arrivals/Arrivals.jsx";
-import Featured from "@/components/website/homePage/featured/Featured.jsx";
+//import Arrivals from "@/components/website/homePage/arrivals/Arrivals.jsx";
+//import Featured from "@/components/website/homePage/featured/Featured.jsx";
 import Rooms from "@/components/website/homePage/rooms/Rooms.jsx";
 
 function Home() {
@@ -13,8 +13,8 @@ function Home() {
     <MainLayout>
       <Hero />
       <Rooms />
-      <Featured />
-      <Arrivals />
+      {/*<Featured />
+      <Arrivals />*/}
       <SalesBanner />
       <Services />
       <Testimonials />
